9384bd99af355cdcedb8ce0da422807802b0e2c6
boni <inkey.cherry@gmail.com>
2023-02-26 18:38:55 +0300

    Update code

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main (int argc, char** argv) {
-    string name; // Имя пользователя
-    std::cin >> name; // Ввод имени пользователя
+    string name;                                      // Имя пользователя
+    std::cin >> name;                                 // Ввод имени пользователя
     std::cout << "Hello world from " << name << endl; // Вывод данных
 }
