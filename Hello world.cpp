5998c74d13999351b5f84ce8d2bd0547a353e937
boni <inkey.cherry@gmail.com>
2023-02-26 18:25:55 +0300

    Fix from master

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main (int argc, char** argv) {
-    string name; // Name of @user
-    std::cin >> name; // Input name of @user
-    std::cout << "Hello world from " << name << endl; // Output name of @user
+    string name; // Имя пользователя
+    std::cin >> name; // Ввод имени пользователя
+    std::cout << "Hello world from " << name << endl; // Вывод данных
 }
