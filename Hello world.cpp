3b40f3380b7cdf244f387c169d4f0191914ccbfe
boni <inkey.cherry@gmail.com>
2023-02-26 18:04:38 +0300

    Changed code style

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
-int main (int argc, char** argv) {
-    string name; // Name of @user
-    std::cin >> name; // Input name of @user
-    std::cout << "Hello world from " << name << endl; // Output name of @user
+int
+main(int argc, char** argv)
+{
+  string name;                                      // Name of @user
+  std::cin >> name;                                 // Input name of @user
+  std::cout << "Hello world from " << name << endl; // Output name of @user
 }
